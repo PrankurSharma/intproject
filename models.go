9e5e60d0b3e9a3da0a78c758db4105922bcc2837
PrankurSharma <prankur.sharma2018@vitbhopal.ac.in>
2020-10-19 12:51:55 +0530

    Changes-2

--- models.go
+++ models.go
@@ -6,7 +6,7 @@ import "time"
 type Meeting struct {
 	ID     primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Title  string             `json:"title" bson:"title,omitempty"`
-        Participants    *Participants  `json:"participants,omitempty" bson:"participants,omitempty"`
+        Participants    *Participants  `json:"Participants,omitempty" bson:"participants,omitempty"`
         Starttime  time.Time  `json:"_starttime" bson:"_starttime"`
         Endtime  time.Time  `json:"_endtime" bson:"_endtime"`
 }
